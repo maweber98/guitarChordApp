6b935a5e7f26f02b47d459f018409f51dd08571e
maweber98 <weberdesignstudio@gmail.com>
2020-07-27 15:28:56 -0400

    Added navigation and drop down menu to select chords

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const axios = require('axios');
 
-/* GET home page. */
+/* GET index page. */
 router.get('/', async function(req, res, next) {
-  console.log('Hello there!');
+  console.log('Index - Get chords');
 
     const getChords = async (chord) => {
       try {
@@ -16,13 +16,47 @@ router.get('/', async function(req, res, next) {
       }
     }
 
-  let chordResults = await getChords('D');
+  let chordResults = await getChords('G');
   console.log('These are the D chords', chordResults);
 
   res.render('index', 
     { 
       title: 'Guitar Chord App',
       chords: chordResults
+  });
+});
+
+router.get('/getChord/:chord',async function (req, res) {
+  const chord = req.params.chord;
+  // res.send('Get chord page');
+  console.log('chord is ', chord);
+
+  const getChords = async (chord) => {
+    try {
+      let response = await axios.get(`https://api.uberchord.com/v1/chords?nameLike=${chord}`)
+      let chords = response.data;
+      return chords;
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  let chordResults = await getChords(chord);
+  console.log('These are the chords', chordResults);
+
+  res.render('getChords', 
+    { 
+      title: 'Guitar Chord App',
+      chords: chordResults
+  });
+});
+
+router.get('/findLikeChords/', function (req, res) {
+  console.log('Find like chords');
+
+  res.render('findLikeChords', 
+    { 
+      title: 'Guitar Chord App'
   });
 });
 
